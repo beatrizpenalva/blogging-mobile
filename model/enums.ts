eedb998c7ac501867541f4f310c0cbf556da2ccd
beatriz-penalva <biapenalva@gmail.com>
2024-11-22 22:08:39 -0300

    feat: create register base form

--- model/enums.ts
+++ model/enums.ts
@@ -2,3 +2,8 @@ export enum UserProfile {
     admin = 'admin',
     viewer = 'viewer'
 }
+
+export const UserProfileLabels = {
+    [UserProfile.admin]: 'Administrativo',
+    [UserProfile.viewer]: 'Discente',
+}
