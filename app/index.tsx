c9b1e10d0c312183d0729965c751c450b464f771
beatriz-penalva <biapenalva@gmail.com>
2024-11-24 18:04:09 -0300

    feat: add  tab bar

--- app/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-import RootLayout from "./_layout"
-
-const App = () => <RootLayout />
-
-export default App
--- app/routes/app.routes.tsx
+++ app/routes/app.routes.tsx
@@ -1,49 +1,80 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-
+import { StyleSheet } from "react-native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Ionicons from "react-native-vector-icons/Ionicons"
 import CreatePost from "../screens/CreatePost"
 import Login from "../screens/Login"
 import Register from "../screens/Register"
 import Timeline from "../screens/Timeline"
 
-const { Navigator, Screen } = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
 
 const AppRoutes = () => {
     return (
-        <Navigator initialRouteName="screens/Login/index">
-            <Screen
-                name="screens/CreatePost/index"
-                component={CreatePost}
-                options={{
-                    title: "Nova publicação",
-                    headerStyle: { backgroundColor: "#276cb1" }
-                }}
-            />
-            <Screen
-                name="screens/Login/index"
-                component={Login}
-                options={{
-                    title: "Entrar",
-                    headerStyle: { backgroundColor: "#276cb1" }
-                }}
-            />
-            <Screen
-                name="screens/Register/index"
-                component={Register}
-                options={{
-                    title: "Cadastro",
-                    headerStyle: { backgroundColor: "#276cb1" }
-                }}
-            />
-            <Screen
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: styles.tabBar,
+                tabBarActiveTintColor: "#fafafa",
+                tabBarInactiveTintColor: "#B0B0B0",
+            }}
+        >
+            <Tab.Screen
                 name="screens/Timeline/index"
                 component={Timeline}
                 options={{
+                    tabBarLabel: "Início",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home" color={color} size={size} />
+                    ),
                     title: "Linha do tempo",
                     headerStyle: { backgroundColor: "#276cb1" }
                 }}
             />
-        </Navigator>
+            <Tab.Screen
+                name="screens/CreatePost/index"
+                component={CreatePost}
+                options={{
+                    tabBarLabel: "Postar",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="create" color={color} size={size} />
+                    ),
+                    title: "Nova publicação",
+                    headerStyle: { backgroundColor: "#276cb1" }
+                }}
+            />
+            <Tab.Screen
+                name="screens/Register/index"
+                component={Register}
+                options={{
+                    tabBarLabel: "Cadastro",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="person" color={color} size={size} />
+                    ),
+                    title: "Novo usuário",
+                    headerStyle: { backgroundColor: "#276cb1" }
+                }}
+            />
+            <Tab.Screen
+                name="screens/Login/index"
+                component={Login}
+                options={{
+                    tabBarLabel: "Sair",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="log-out" color={color} size={size} />
+                    ),
+                    tabBarStyle: { display: "none" },
+                    title: "Login",
+                    headerStyle: { backgroundColor: "#276cb1" }
+                }}
+            />
+        </Tab.Navigator>
     )
 }
 
 export default AppRoutes
+
+const styles = StyleSheet.create({
+    tabBar: {
+        backgroundColor: "#276cb1",
+        height: 60,
+    }
+})
